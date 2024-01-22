a2596220ffbdf57236ddcb953303e525a1948bbc
0s4k4 <soulbosss@gmail.com>
2024-01-21 23:30:51 -0600

    se hacen activiades hasta la partr 4

---
+++ LogicaProgramacion/sumergueteEnProgramacion/horaDePracticarContador/script.js
@@ -0,0 +1,41 @@
+//desaafio 1
+
+// contador1 = 1;
+
+// while( contador1 < 11){
+
+//     console.log(contador1);
+//     contador1 = contador1+1;
+
+// }
+
+
+//desafio2
+
+// contador2 = 10;
+
+// while (contador2 > -1){
+//     console.log(contador2);
+//     contador2 = contador2 - 1;
+// }
+
+
+//desafio3
+
+// let numeroUsuario = Number(prompt('introduzca nmumero'));
+
+// while (numeroUsuario > -1){
+//     console.log(numeroUsuario);
+//     numeroUsuario = numeroUsuario -1;
+// }
+
+
+//desafio4
+
+
+let numeroUsuario2 = Number(prompt('introduzca el numero'));
+let contadorCero = 0
+while(numeroUsuario2 +1 > contadorCero  ) {
+    console.log(contadorCero);
+    contadorCero = contadorCero+1;
+}
