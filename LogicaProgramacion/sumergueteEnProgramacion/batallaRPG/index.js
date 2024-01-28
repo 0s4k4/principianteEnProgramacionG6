33d92bfa6ccb22119f2271eabeb861c6a4a34843
0s4k4 <soulbosss@gmail.com>
2024-01-28 13:53:37 -0600

    se agrega y termina el juego sencillo en js

--- LogicaProgramacion/sumergueteEnProgramacion/batallaRPG/index.js
+++ LogicaProgramacion/sumergueteEnProgramacion/batallaRPG/index.js
@@ -5,7 +5,9 @@ alert('Un enemigo misterioso te ha asechado, has iniciado la batalla...');
 let vidaJugador = 100;
 let vidaEnemigo = 100;
 
-let ataquePersonaje = 10;
+
+
+let ataquePersonaje = 100;
 let ataque_Enemiego = 15;
 let sandwitch = 40;
 //inicializamos el valor de vida de ambos personajes
@@ -19,15 +21,40 @@ while(vidaJugador && vidaEnemigo > 0 ) {
 
     if(opcion == 1){
         alert('seleccionaste la opcion 1, has atacado')
-        
+        vidaEnemigo = vidaEnemigo - ataquePersonaje
+        alert('al enemigo le resta '+vidaEnemigo );
     }
     else if (opcion == 2){
-        alert('has selecionado la opcion 2')
+        alert('has selecionado la opcion de mochila')
+        vidaJugador = vidaJugador + sandwitch;
+        alert('el personaje se ha recuperado, ahora tiene '+vidaJugador+' de vida');
     }
 
     else if (opcion == 3) {
         alert('has seleccionado la opcion 3')
+        alert('te has escadado de la partida');
+        break;
     }
-    
-    
+
+    if (vidaEnemigo <= 0 ){
+        break;
+    }
+
+    alert('es turno del enemigo, el enemigo te ha atacado');
+    vidaJugador = vidaJugador - ataque_Enemiego;
+    alert('ahora tienes ' + vidaJugador);
+
+}
+
+
+
+if(vidaEnemigo <= 0){
+    alert('el enemigo  ha sido derrotado');
+    alert('has ganado la batalla, felicidades, eres un heroe')
+}
+else if(vidaJugador <= 0){
+    alert('el enemigo te ha derrotado');
+    alert('has perdido, has perecido')
+} else {
+    alert('te has escapado');
 }
