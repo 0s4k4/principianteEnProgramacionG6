2cf6bdcf227bc079b111c938230974252fe50d82
soulboss@gmail.com <soulboss@gmail.com>
2024-01-28 11:49:05 -0600

    se añaden cambios del js

--- LogicaProgramacion/sumergueteEnProgramacion/batallaRPG/index.js
+++ LogicaProgramacion/sumergueteEnProgramacion/batallaRPG/index.js
@@ -2,8 +2,12 @@ alert('Te encuentras caminando por el bosque... o espera que encontraste?')
 alert('Ahhh te has encontrado con un terrible destino')
 alert('Un enemigo misterioso te ha asechado, has iniciado la batalla...');
 
-vidaJugador = 100;
-vidaEnemigo = 100;
+let vidaJugador = 100;
+let vidaEnemigo = 100;
+
+let ataquePersonaje = 10;
+let ataque_Enemiego = 15;
+let sandwitch = 40;
 //inicializamos el valor de vida de ambos personajes
 
 while(vidaJugador && vidaEnemigo > 0 ) {
@@ -14,9 +18,16 @@ while(vidaJugador && vidaEnemigo > 0 ) {
     let opcion = Number(prompt('Selecciona tu decision'));
 
     if(opcion == 1){
-        alert('seleccionaste la opcion 1')
+        alert('seleccionaste la opcion 1, has atacado')
+        
+    }
+    else if (opcion == 2){
+        alert('has selecionado la opcion 2')
     }
 
+    else if (opcion == 3) {
+        alert('has seleccionado la opcion 3')
+    }
     
     
 }
